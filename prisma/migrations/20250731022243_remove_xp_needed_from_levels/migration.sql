2001c89ad84b7b4ffa973e0154b782ffe48d538a
Wellington Alves <wellington.alves95@gmail.com>
2025-12-07 18:50:46 -0300

    Refactor code structure for improved readability and maintainability

--- prisma/migrations/20250731022243_remove_xp_needed_from_levels/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `xpNeeded` on the `level` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `level` DROP COLUMN `xpNeeded`;
